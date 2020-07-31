8c2c7ba47cfebaf6eb9d9b42e431c2e1bd3de185
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-07-31 16:21:56 -0700

    Nasty_Surprise

--- src/_01_nasty_surprise/Nasty_Surprise.java
+++ src/_01_nasty_surprise/Nasty_Surprise.java
@@ -13,13 +13,19 @@ public class Nasty_Surprise {
 	public static void main(String[] args) {
 		showPictureFromTheInternet("https://thehappypuppysite.com/wp-content/uploads/2015/01/Dollarphotoclub_68650327-300x299.jpg");
 	}
-	private void showPictureFromTheInternet(String imageUrl) {
+	private static void showPictureFromTheInternet(String imageUrl) {
 	    try {
 	        URL url = new URL(imageUrl);
 	        Icon icon = new ImageIcon(url);
 	        JLabel imageLabel = new JLabel(icon);
 	        JFrame frame = new JFrame();
+	        JButton button1 = new JButton("Treat");
+	        button1.setSize(50, 50);
+	        JButton button2 = new JButton("Trick");
+	        button2.setSize(50, 50);
 	        frame.add(imageLabel);
+	        frame.add(button1);
+	        frame.add(button2);
 	        frame.pack();
 	        frame.setVisible(true);
 	    } catch (MalformedURLException e) {
