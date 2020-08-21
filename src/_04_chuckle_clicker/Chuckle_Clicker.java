48e2873bb0f52159c8f6e89d23e0c85d66190f9f
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-08-21 16:41:35 -0700

    Chuckle_Clicker(IP)

---
+++ src/_04_chuckle_clicker/Chuckle_Clicker.java
@@ -0,0 +1,18 @@
+package _04_chuckle_clicker;
+
+import java.awt.Button;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Chuckle_Clicker {
+public static void main(String[] args) {
+}
+
+private void makeButtons() {
+	// TODO Auto-generated method stub
+	JOptionPane.showMessageDialog(null, "Make Buttons");
+
+}
+
+}
