f0bb4ec66a503b304889ccf1d8b7f5da48b7f1a7
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-08-28 16:40:13 -0700

    Typing Tutor (IP)

--- src/_05_typing_tutor/Typing_Tutor.java
+++ src/_05_typing_tutor/Typing_Tutor.java
@@ -1,5 +1,11 @@
 package _05_typing_tutor;
 
+import javax.swing.JFrame;
+
 public class Typing_Tutor {
 
+	public void createGUI() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+	}
 }
---
+++ src/_05_typing_tutor/Typing_Tutor_Runner.java
@@ -0,0 +1,8 @@
+package _05_typing_tutor;
+
+public class Typing_Tutor_Runner {
+	public static void main(String[] args) {
+		Typing_Tutor t = new Typing_Tutor();
+		t.createGUI();
+	}
+}
