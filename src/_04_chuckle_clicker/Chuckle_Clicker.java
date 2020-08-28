c24678da4098997b7d3a43f434dc6f73d7cb1863
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-08-28 16:24:08 -0700

    Chuckle_Clicker (FINISHED)

--- src/_04_chuckle_clicker/Chuckle_Clicker.java
+++ src/_04_chuckle_clicker/Chuckle_Clicker.java
@@ -1,18 +1,41 @@
 package _04_chuckle_clicker;
 
 import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class Chuckle_Clicker {
-public static void main(String[] args) {
-}
+public class Chuckle_Clicker implements ActionListener {
+	JButton b1;
+	JButton b2;
 
-private void makeButtons() {
-	// TODO Auto-generated method stub
-	JOptionPane.showMessageDialog(null, "Make Buttons");
+	public void makeButtons() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		b1 = new JButton("Joke");
+		panel.add(b1);
+		b1.addActionListener(this);
+		b2 = new JButton("Punchline");
+		panel.add(b2);
+		b2.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
+	}
 
-}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == b1) {
+			JOptionPane.showMessageDialog(null, "Why do we tell actors to break a leg?");
+		} else if (e.getSource() == b2) {
+			JOptionPane.showMessageDialog(null, "Because every play has a cast.");
+		}
+
+	}
 
 }
---
+++ src/_04_chuckle_clicker/Chuckle_Clicker_Runner.java
@@ -0,0 +1,8 @@
+package _04_chuckle_clicker;
+
+public class Chuckle_Clicker_Runner {
+	public static void main(String[] args) {
+		Chuckle_Clicker chuck = new Chuckle_Clicker();
+		chuck.makeButtons();
+	}
+}
