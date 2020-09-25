647873b521728fb2eb207fb03dc4a12c4158720e
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-09-25 16:41:10 -0700

    Latest_Tweet(IP)

---
+++ src/_09_latest_tweet/Latest_Tweet.java
@@ -0,0 +1,59 @@
+package _09_latest_tweet;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Latest_Tweet implements ActionListener {
+JTextField t1;
+JButton b1;
+JLabel label = new JLabel();
+
+/*private String getLatestTweet(String searchingFor) {
+
+    Twitter twitter = new TwitterFactory().getInstance();
+
+    AccessToken accessToken = new AccessToken(
+        "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+        "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+        "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+    twitter.setOAuthAccessToken(accessToken);
+
+Query query = new Query(searchingFor);
+    try {
+        QueryResult result = twitter.search(query);
+        return result.getTweets().get(0).getText();
+    } catch (Exception e) {
+        System.err.print(e.getMessage());
+        return "What the heck is that?";
+    }
+}*/
+
+	public void run() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		t1 = new JTextField(15);
+		panel.add(t1);
+		b1 = new JButton("Search the Twitterverse");
+		panel.add(b1);
+		b1.addActionListener(this);
+		panel.add(label);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==b1) {
+			System.out.println("Tweet Tweet");
+		}
+	}
+}
---
+++ src/_09_latest_tweet/Latest_Tweet_Runner.java
@@ -0,0 +1,7 @@
+package _09_latest_tweet;
+
+public class Latest_Tweet_Runner {
+	public static void main(String[] args) {
+		new Latest_Tweet().run();
+	}
+}
