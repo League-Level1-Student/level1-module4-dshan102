7883eac0a6c9d01c372d089055b68af0712f7706
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-11-06 17:42:29 -0800

    Whack_A_Mole (FINISHED)

--- src/_11_whack_a_mole/Whack_A_Mole.java
+++ src/_11_whack_a_mole/Whack_A_Mole.java
@@ -3,17 +3,21 @@ package _11_whack_a_mole;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Whack_A_Mole implements ActionListener {
 	JFrame frame = new JFrame("Whack-a-mole");
 	JPanel panel = new JPanel();
+	Date timeAtStart = new Date(); 
+	static int moleHits = 0;
 	
 	public void run() {
 		frame.setPreferredSize(new Dimension(200, 200));
@@ -24,6 +28,7 @@ public class Whack_A_Mole implements ActionListener {
 	}
 	
 	public void drawButtons() {
+		panel.removeAll();
 		Random rand = new Random();
 		int rand1 = rand.nextInt(24);
 		for(int i=0; i<24; i++) {
@@ -53,6 +58,13 @@ public class Whack_A_Mole implements ActionListener {
             }
         }
     }
+	
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+	    Date timeAtEnd = new Date();
+	    JOptionPane.showMessageDialog(null, "Your whack rate is "
+	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	                  + " moles per second.");
+	}
 
 
 	@Override
@@ -60,8 +72,17 @@ public class Whack_A_Mole implements ActionListener {
 		// TODO Auto-generated method stub
 		JButton buttonPressed = (JButton) e.getSource();
 		if(! buttonPressed.getText().equals("Mole")) {
+			moleHits++;
+			frame.dispose();
+			run();
+		}
+		else {
 			speak("Fat L");
 		}
+		if (moleHits==10) {
+			endGame(timeAtStart, 10);
+		}
+
 	}
 }
 
--- src/_11_whack_a_mole/Whack_A_Mole_Runner.java
+++ src/_11_whack_a_mole/Whack_A_Mole_Runner.java
@@ -1,5 +1,7 @@
 package _11_whack_a_mole;
 
+import java.util.Date;
+
 public class Whack_A_Mole_Runner {
 public static void main(String[] args) {
 	Whack_A_Mole mole = new Whack_A_Mole();
