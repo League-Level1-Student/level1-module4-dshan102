a9815fbc52cb88f2194d949e780c59416723085e
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-11-13 18:21:54 -0800

    Slot_Machine(FINISHED), Practice_Test(FINISHED)

--- src/_12_slot_machine/Slot_Machine.java
+++ src/_12_slot_machine/Slot_Machine.java
@@ -5,36 +5,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Slot_Machine implements ActionListener {
-	JFrame frame = new JFrame();
+	JFrame frame = new JFrame("Slot Machine");
 	JLabel label = new JLabel();
+	JPanel panel = new JPanel();
 	JButton b1;
 	JLabel label1;
 	JLabel label2;
 	JLabel label3;
-	
-	private JLabel createLabelImage(String fileName) throws MalformedURLException{
-        URL imageURL = getClass().getResource(fileName);
-	if (imageURL == null){
-		System.err.println("Could not find image " + fileName);
-		return new JLabel();
-	}
-	Icon icon = new ImageIcon(imageURL);
-	JLabel imageLabel = new JLabel(icon);
-	return imageLabel;
-}
-    
-	
+   
 	public void run() throws MalformedURLException {
-		JPanel panel = new JPanel();
 		frame.add(panel);
 		label1 = createLabelImage("cherry.jpg");
 		label1.setPreferredSize(new Dimension(200,200));
@@ -52,12 +42,92 @@ public class Slot_Machine implements ActionListener {
 		panel.add(label);
 		frame.setVisible(true);
 	}
+	public void spin() throws MalformedURLException {
+		Random rand = new Random();
+		int option1 = rand.nextInt(3);
+		int option2 = rand.nextInt(3);
+		int option3 = rand.nextInt(3);
+		
+		if(option1==0) {
+			panel.remove(label1);
+			label1=createLabelImage("cherry.jpg");
+			label1.setPreferredSize(new Dimension(200,200));
+			panel.add(label1);
+		}
+		if(option1==1) {
+			panel.remove(label1);
+			label1=createLabelImage("orange-512.png");
+			label1.setPreferredSize(new Dimension(200,200));
+			panel.add(label1);
+		}
+		if(option1==2) {
+			panel.remove(label1);
+			label1=createLabelImage("watermelon.png");
+			label1.setPreferredSize(new Dimension(200,200));
+			panel.add(label1);
+		}
+		if(option2==0) {
+			panel.remove(label2);
+			label2=createLabelImage("cherry.jpg");
+			label2.setPreferredSize(new Dimension(200,200));
+			panel.add(label2);
+		}
+		if(option2==1) {
+			panel.remove(label2);
+			label2=createLabelImage("orange-512.png");
+			label2.setPreferredSize(new Dimension(200,200));
+			panel.add(label2);
+		}
+		if(option2==2) {
+			panel.remove(label2);
+			label2=createLabelImage("watermelon.png");
+			label2.setPreferredSize(new Dimension(200,200));
+			panel.add(label2);
+		}
+		if(option3==0) {
+			panel.remove(label3);
+			label3=createLabelImage("cherry.jpg");
+			label3.setPreferredSize(new Dimension(200,200));
+			panel.add(label3);
+		}
+		if(option3==1) {
+			panel.remove(label3);
+			label3=createLabelImage("orange-512.png");
+			label3.setPreferredSize(new Dimension(200,200));
+			panel.add(label3);
+		}
+		if(option3==2) {
+			panel.remove(label3);
+			label3=createLabelImage("watermelon.png");
+			label3.setPreferredSize(new Dimension(200,200));
+			panel.add(label3);
+		}
+		if(option1==option2 && option2==option3 && option1==option3) {
+			JOptionPane.showMessageDialog(null, "YOU WIN");
+		}
+	}
 
+	private JLabel createLabelImage(String fileName) throws MalformedURLException{
+        URL imageURL = getClass().getResource(fileName);
+	if (imageURL == null){
+		System.err.println("Could not find image " + fileName);
+		return new JLabel();
+	}
+	Icon icon = new ImageIcon(imageURL);
+	JLabel imageLabel = new JLabel(icon);
+	return imageLabel;
+}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getSource()==b1) {
-			
+			try {
+				spin();
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			frame.pack();
 		}
 	}
 }
